53941e41c6fe00d417d810570572ac774cdf5f3e
Wagner Trezub <60133113+Wagner3UB@users.noreply.github.com>
2021-07-14 11:50:31 +0200

    Add files via upload
    add file temperatureConverter.js

---
+++ temperatureConverter.js
@@ -0,0 +1,13 @@
+function temperature(celsius){
+  if(typeof celsius !== "number"){
+    return "Write a valid number";
+  } else if (celsius < -232){
+    return "impossible to calculate, temperature under absolute zero - 0 Kelvin";
+  } else {
+  return (celsius * 1.8 + 32) + "°F or " + (celsius + 273.15) + "K"; //first one is the farenheit formula and after that the kelvin formula
+  
+  };
+};
+
+let result = temperature(36); //Here you can change the CELSIUS temperature
+console.log(result);
